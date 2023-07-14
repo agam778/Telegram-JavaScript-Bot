b1b8dbc113dc294c75e4799cf18537ce29705c5c
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-07-14 15:41:34 +0530

    feat: Add 8ball command

---
+++ commands/8ball.js
@@ -0,0 +1,41 @@
+const responses = [
+  "It is certain.",
+  "Without a doubt.",
+  "You may rely on it.",
+  "Yes, definitely.",
+  "It is decidedly so.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Signs point to yes.",
+  "Absolutely!",
+  "Don't count on it.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+  "Not likely.",
+  "Better not tell you now.",
+  "Chances are low.",
+  "Unlikely.",
+  "No way LMAO.",
+  "I highly doubt it.",
+];
+
+module.exports = {
+  name: "8ball",
+  description: "Ask the magic 8-ball a question",
+  usage: "/8ball <question>",
+  example: "/8ball Is this bot awesome?",
+  category: "Fun",
+  handler: async (ctx) => {
+    const { message } = ctx;
+    const { text } = message;
+
+    if (text === "/help" || text === `/help@${ctx.me.username}`) {
+      const response = responses[Math.floor(Math.random() * responses.length)];
+      await ctx.reply(response);
+    } else {
+      await ctx.reply("You need to ask a question!");
+    }
+  },
+};
